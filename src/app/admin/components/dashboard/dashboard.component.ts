1a751ff20afe73df21b8b5cdf49f457990db71ee
Uygar Eren <uygar_eren2000@hotmail.com>
2024-01-01 17:37:45 +0300

    Added CustomToastr service.

--- src/app/admin/components/dashboard/dashboard.component.ts
+++ src/app/admin/components/dashboard/dashboard.component.ts
@@ -1,10 +1,20 @@
+import {
+  CustomtoastrService,
+  ToastrMessageType,
+} from './../../../services/alerts/customtoastr.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
+  constructor(private toastr: CustomtoastrService) {}
 
+  showAlert() {
+    this.toastr.message('Example Alert', 'Admin Dashboard', {
+      messageType: ToastrMessageType.Success,
+    });
+  }
 }
---
+++ src/app/services/alerts/customtoastr.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { IndividualConfig, Toast, ToastrService } from 'ngx-toastr';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CustomtoastrService {
+  constructor(private toastr: ToastrService) {}
+  message(message: string, title: string, options: Partial<ToastrOptions>) {
+    this.toastr[options.messageType!](message, title);
+  }
+}
+
+export class ToastrOptions {
+  messageType: ToastrMessageType = ToastrMessageType.Success;
+  position: ToastrPosition = ToastrPosition.TopRight;
+  delay: number = 5;
+}
+
+export enum ToastrMessageType {
+  Error = 'error',
+  Info = 'info',
+  Success = 'success',
+  Warning = 'warning',
+}
+
+export enum ToastrPosition {
+  TopRight = 'toast-top-right',
+  TopCenter = 'toast-top-center',
+  TopLeft = 'toast-top-let',
+  TopFullWidth = 'toast-top-full-width',
+  BottomRight = 'toast-bottom-right',
+  BottomCenter = 'toast-bottom-center',
+  BottomLeft = 'toast-bottom-left',
+  BottomFullWidth = 'toast-bottom-full-width',
+}

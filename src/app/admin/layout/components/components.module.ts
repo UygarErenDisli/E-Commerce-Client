9bff62f452498ab4620a2ce29982e07eecd08900
Uygar Eren <uygar_eren2000@hotmail.com>
2024-01-01 16:52:31 +0300

    Added simple admin dashboard with Angular Meterial.

--- src/app/admin/layout/components/components.module.ts
+++ src/app/admin/layout/components/components.module.ts
@@ -4,9 +4,12 @@ import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
 
+import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
+
 @NgModule({
   declarations: [HeaderComponent, SidebarComponent, FooterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, MatListModule],
   exports: [HeaderComponent, SidebarComponent, FooterComponent],
 })
 export class ComponentsModule {}

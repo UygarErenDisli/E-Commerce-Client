6e69cdcb076e4c0464695a66cc6ef402a4a33d38
Uygar Eren <uygar_eren2000@hotmail.com>
2024-01-04 17:43:01 +0300

    Added simple data table with pagination to Admin-Product Page
    And also updated product.module.ts for required modules to implement data table to listing products

--- src/app/admin/components/products/list/list.component.ts
+++ src/app/admin/components/products/list/list.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  SpinnerComponent,
+  SpinnerType,
+} from '../../../../base/spinner/spinner.component';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ProductService } from '../../../../services/common/models/product.service';
+import {
+  CustomToastrService,
+  ToastrMessageType,
+  ToastrPosition,
+} from '../../../../services/alerts/customtoastr.service';
+import { ListProduct, ListProducts } from '../../../../contracts/list-products';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrl: './list.component.scss'
+  styleUrl: './list.component.scss',
 })
-export class ListComponent {
+export class ListComponent extends SpinnerComponent implements OnInit {
+  displayedColumns: string[] = [
+    'name',
+    'stock',
+    'price',
+    'createdDate',
+    'updatedDate',
+  ];
+  dataSource: MatTableDataSource<ListProduct> = new MatTableDataSource();
 
+  @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
+
+  constructor(
+    spinner: NgxSpinnerService,
+    private productService: ProductService,
+    private toastrAlert: CustomToastrService
+  ) {
+    super(spinner);
+  }
+  async getProducts() {
+    this.showSpinner(SpinnerType.BallSpin);
+    const listProducts: ListProducts | undefined =
+      await this.productService.read(
+        this.paginator ? this.paginator.pageIndex : 0,
+        this.paginator ? this.paginator.pageSize : 5,
+        () => {
+          this.hideSpinner(SpinnerType.BallSpin);
+        },
+        (errorMessage) => {
+          this.toastrAlert.message(errorMessage, 'Products', {
+            messageType: ToastrMessageType.Error,
+            position: ToastrPosition.TopRight,
+          });
+        }
+      );
+    this.dataSource = new MatTableDataSource<ListProduct>(
+      listProducts?.products
+    );
+
+    this.paginator!.length = listProducts?.totalCount;
+  }
+  async pageChanged() {
+    await this.getProducts();
+  }
+  async ngOnInit(): Promise<void> {
+    await this.getProducts();
+  }
 }
